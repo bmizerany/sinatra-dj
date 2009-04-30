8e4edb8b0837bd37a676b07d5b1eb6301eec57a8
Blake Mizerany <blake.mizerany@gmail.com>
2009-04-29 23:09:58 -0700

    captcha

--- sinatra-dj.rb
+++ sinatra-dj.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift 'vendor/delayed_job/lib'
 require 'sinatra'
 require 'activerecord'
 require 'delayed_job'
+require 'open-uri'
 
 configure do
   config = YAML::load(File.open('config/database.yml'))
@@ -36,12 +37,24 @@ class Translation < ActiveRecord::Base
   end
 end
 
+helpers do
+  def captcha_pass?(session, answer)
+    session = session.to_i
+    answer  = answer.gsub(/\W/, '')
+    open("http://captchator.com/captcha/check_answer/#{session}/#{answer}").read.to_i.nonzero? rescue false
+  end
+end
+
 get '/' do
+  @session = rand(1000) + 1000
   @translations = Translation.all(:order => 'created_at desc')
   erb :translations
 end
 
 post '/translations' do
+  unless captcha_pass?(params[:session], params[:answer])
+    halt 401, "Invalid Captcha Answer"
+  end
   Translation.create! :input => params[:input]
   redirect '/'
 end
@@ -71,5 +84,8 @@ __END__
 <h2>New Translation</h2>
 <form method="post" action="/translations">
   <div><textarea rows="3" cols="80" name="input">Enter text to translate</textarea></div>
+  <div><img src="http://captchator.com/captcha/image/<%= @session %>"/></p>
+  <p><input name="session" type="hidden" value="<%= @session %>"/></p>
+  <p><input name="answer" type="text" size="10"/></div>
   <div><input type="submit" value="Submit" /></div>
 </form>
