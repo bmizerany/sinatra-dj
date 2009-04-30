84f9b813ef1346d0f9004ae058578cc3067e2871
Blake Mizerany <blake.mizerany@gmail.com>
2009-04-29 22:30:19 -0700

    seperation unneeded

--- sinatra-dj.rb
+++ sinatra-dj.rb
@@ -15,6 +15,29 @@ configure do
   )
 end
 
+class Translation < ActiveRecord::Base
+  after_create :queue
+
+  def queue
+    Delayed::Job.enqueue self
+  end
+
+  def perform
+    self.output = self.class.english_to_pig_latin(input)
+    save!
+  end
+
+  def self.english_to_pig_latin(text)
+    text.split.map do |word|
+      if word.length <= 2
+        word
+      else
+        (word[1,9999] + word[0,1] + "ay").downcase
+      end
+    end.join(" ")
+  end
+end
+
 get '/' do
   @translations = Translation.all(:order => 'created_at desc')
   erb :translations
